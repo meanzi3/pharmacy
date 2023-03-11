506d9f1cf0aff5aaf8f9f76141dc812db8be862c
Minji <minjichoe0326@gmail.com>
2023-03-11 23:32:11 +0900

    약국 엔티티, 리포지토리 생성
    테스트 전, 약국 엔티티와 리포지토리(JPA이용)를 생성하였다.

---
+++ src/main/java/com/example/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,7 @@
+package com.example.pharmacy.repository;
+
+import com.example.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+}
